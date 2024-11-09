f639d35e26c833fc7417b04da7e4cc046364c751
Yaqub <yaqub399@my.yorku.ca>
2024-11-08 23:47:09 -0500

    Added MaterialUi to the Buttons
    Changed the buttons and used materialui to make it. Changed the color to a dark theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import routes from '../routes';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 function Navbar() {
     const navigate = useNavigate();
     return (
         <nav>
-        <button onClick={() => navigate(routes.home)}>Home</button>
-        <button onClick={() => navigate(routes.about)}>About</button>
-        <button onClick={() => navigate(routes.contact)}>Contact</button>
-    </nav>
+            <Stack direction="row" spacing={2}>
+                <Button variant="contained" sx={{ backgroundColor: '#212b35', color: '#fff' }}  onClick={() => navigate(routes.home)}>
+                    Home
+                </Button>
+                <Button variant="contained" sx={{ backgroundColor: '#212b35', color: '#fff' }} onClick={() => navigate(routes.about)}>
+                    About
+                </Button>
+                <Button variant="contained" sx={{ backgroundColor: '#212b35', color: '#fff' }} onClick={() => navigate(routes.contact)}>
+                    Contact
+                </Button>
+            </Stack>
+        </nav>
     );
 }
 
