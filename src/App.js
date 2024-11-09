39464674556f586913af8e7bb962425b19b15343
Taha Shah <94569076+tahas022@users.noreply.github.com>
2024-11-08 23:35:35 -0500

    Typescriptconversion (#1)
    * KAN-23: Converted all JS files into TS
    
    * refactoring code
    
    * Refactoring code
    
    * Converted all base functions to arrow functions
    
    ---------
    
    Co-authored-by: Taha Shahid <90358100+tahas02@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import About from "./pages/About.tsx";
+import Contact from "./pages/Contact.tsx";
+import Navbar from "./components/Navbar.tsx";
 
-function App() {
+const App = () => {
   return (
     <Router>
       <div>
         <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
