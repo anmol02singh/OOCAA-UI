a91f4067c0504e373a9a22fe49732445a0cb9181
Taha Shahid <90358100+tahas02@users.noreply.github.com>
2024-11-10 14:05:39 -0500

    Creating styling page

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import Home from "./pages/Home.tsx";
 import About from "./pages/About.tsx";
 import Contact from "./pages/Contact.tsx";
 import Navbar from "./components/Navbar.tsx";
-import routes from './routes';
-
+import routes from "./routes";
+import UserLogin from "./pages/UserLogin.tsx";
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Navbar />
         <Routes>
-                <Route path={routes.home} element={<Home />} />
-                <Route path={routes.about} element={<About />} />
-                <Route path={routes.contact} element={<Contact />} />
-            </Routes>
+          <Route path="/login" element={<UserLogin />} />
+        </Routes>
+        {/* <Navbar /> */}
+        <Routes>
+          <Route path={routes.home} element={<Home />} />
+          <Route path={routes.about} element={<About />} />
+          <Route path={routes.contact} element={<Contact />} />
+        </Routes>
       </div>
     </Router>
   );
