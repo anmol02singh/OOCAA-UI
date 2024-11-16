fe49bf092b835fabe3c6a607e164192877972f95
Natalie Dean <natalierdean1@gmail.com>
2024-11-15 23:21:11 -0500

    Fixed navbar layout setup for login page.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Box from '@mui/material/Box';
 import Home from "./pages/Home.tsx";
 import About from "./pages/About.tsx";
 import Contact from "./pages/Contact.tsx";
-import Navbar from "./components/Navbar.tsx";
 import routes from "./routes";
-import UserLogin from "./pages/UserLogin.tsx";import { NavbarProvider } from "./components/NavbarContext.tsx";
-
-const PageContainer = {
-  padding: '1em',
-  overflowX: 'auto',
-  whiteSpace: 'pre-wrap',
-  wordWrap: 'break-word',
-  maxWidth: '100%',
-};
+import UserLogin from "./pages/UserLogin.tsx";
+import NavbarLayout from "./components/NavbarLayout.tsx";
 
 const App = () => {
   return (
+
     <Router>
       <div>
         <Routes>
           <Route path="/login" element={<UserLogin />} />
         </Routes>
-        <Navbar />
-        <Routes>
-                <Route path={routes.home} element={<Home />} />
-                <Route path={routes.about} element={<About />} />
-                <Route path={routes.contact} element={<Contact />} />
-            </Routes>
       </div>
+      <Routes>
+        <Route
+          path={routes.home}
+          element={
+            <NavbarLayout>
+              <Home />
+            </NavbarLayout>
+          }
+        />
+        <Route
+          path={routes.about}
+          element={
+            <NavbarLayout>
+              <About />
+            </NavbarLayout>
+          }
+        />
+        <Route
+          path={routes.contact}
+          element={
+            <NavbarLayout>
+              <Contact />
+            </NavbarLayout>
+          }
+        />
+      </Routes>
     </Router>
   );
 };
