08cca2af9b4e3c1adfbf40cc1237acdbd6b772b7
NatalieRDean <57269944+NatalieRDean@users.noreply.github.com>
2024-11-19 15:19:26 -0500

    Kan 28/navigation bar (#6)
    * Set up main nav bar functionality and most of layout.
    
    * Added home routing to logo
    
    * Added material icons to dependencies, made navbar push over pages and maintained it's state within a session.
    
    * Updated the style of the navbar and its components
    
    * Converted inline styling to CSS and moved it into a CSS file.
    
    * Fixed navbar layout setup for login page.
    
    * Tweaked style and make website logo a placeholder icon.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import About from "./pages/About.tsx";
 import Contact from "./pages/Contact.tsx";
-import Navbar from "./components/Navbar.tsx";
 import routes from "./routes";
 import UserLogin from "./pages/UserLogin.tsx";
+import NavbarLayout from "./components/NavbarLayout.tsx";
 
 const App = () => {
   return (
+
     <Router>
       <div>
         <Routes>
           <Route path="/login" element={<UserLogin />} />
         </Routes>
-        {/* <Navbar /> */}
-        <Routes>
-          <Route path={routes.home} element={<Home />} />
-          <Route path={routes.about} element={<About />} />
-          <Route path={routes.contact} element={<Contact />} />
-        </Routes>
       </div>
+      <Routes>
+        <Route
+          path={routes.home}
+          element={
+            <NavbarLayout>
+              <Home />
+            </NavbarLayout>
+          }
+        />
+        <Route
+          path={routes.about}
+          element={
+            <NavbarLayout>
+              <About />
+            </NavbarLayout>
+          }
+        />
+        <Route
+          path={routes.contact}
+          element={
+            <NavbarLayout>
+              <Contact />
+            </NavbarLayout>
+          }
+        />
+      </Routes>
     </Router>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,107 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import routes from '../routes';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import Logo from '@mui/icons-material/SatelliteAlt';
+import './Navbar.css';
 
-function Navbar() {
+import MuiDrawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import HomeIcon from '@mui/icons-material/Home';
+import AboutIcon from '@mui/icons-material/Info';
+import ContactIcon from '@mui/icons-material/PermContactCalendar';
+import ListSubheader from '@mui/material/ListSubheader';
+import routes from '../routes';
+import { useNavbar } from './NavbarContext.tsx';
+import { Typography } from '@mui/material';
+
+const NavbarItems = [
+    { kind: 'divider' },
+    { kind: 'header', title: 'Overview' },
+    { segment: 'home', title: 'Home', icon: <HomeIcon /> },
+    { segment: 'about', title: 'About', icon: <AboutIcon /> },
+    { segment: 'contact', title: 'Contact', icon: <ContactIcon /> },
+    { kind: 'divider' },
+];
+
+const Navbar = () => {
+    const { isNavbarOpen, toggleNavbar } = useNavbar();
+
     const navigate = useNavigate();
+
     return (
-        <nav>
-            <Stack direction="row" spacing={2}>
-                <Button variant="contained" sx={{ backgroundColor: '#212b35', color: '#fff' }}  onClick={() => navigate(routes.home)}>
-                    Home
-                </Button>
-                <Button variant="contained" sx={{ backgroundColor: '#212b35', color: '#fff' }} onClick={() => navigate(routes.about)}>
-                    About
-                </Button>
-                <Button variant="contained" sx={{ backgroundColor: '#212b35', color: '#fff' }} onClick={() => navigate(routes.contact)}>
-                    Contact
-                </Button>
-            </Stack>
-        </nav>
+        <MuiDrawer
+            className={`navbarDrawer ${isNavbarOpen ? 'open' : ''}`}
+            variant="permanent"
+            anchor="left"
+            open={isNavbarOpen}
+            onClose={toggleNavbar}
+        >
+            <div className='navbarDrawerHeader'>
+                {/* <img
+                    className='logo'
+                    src={Logo}
+                    alt="rocketship"
+                    onClick={() => navigate(routes.home)}
+                /> */}
+                <Logo
+                    className='logo'
+                    onClick={() => navigate(routes.home)}
+                />
+                <IconButton
+                    className={`navbarDrawerButton ${isNavbarOpen ? 'open' : ''}`}
+                    onClick={toggleNavbar}
+                >
+                    {isNavbarOpen ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                </IconButton>
+            </div>
+            <List className='navbarList'>
+                {NavbarItems.map((item) => {
+                    if (item == null) {
+                        return null;
+                    }
+                    if (item.kind === 'header') {
+                        return (
+                            <ListSubheader
+                                className={`navbarSubheader ${isNavbarOpen ? 'open' : ''}`}
+                                key={item.title}
+                            >
+                                <Typography className='navbarSubheaderText'>
+                                    {item.title}
+                                </Typography>
+                            </ListSubheader>
+                        );
+                    }
+                    if (item.kind === 'divider') {
+                        return <Divider
+                            className="navbarDivider"
+                            key={item.title}
+                        />;
+                    }
+                    return (
+                        <ListItem
+                            className='navbarListItem'
+                            key={item.segment}
+                        >
+                            <ListItemButton
+                                className={`navbarItemButton ${isNavbarOpen ? 'open' : ''}`}                                
+                                onClick={() => navigate(routes[item.segment])}>
+                                <ListItemIcon className={`navbarItemIcon ${isNavbarOpen ? 'open' : ''}`}>
+                                    {item.icon}
+                                </ListItemIcon>
+                                {isNavbarOpen && <ListItemText primary={item.title} />}
+                            </ListItemButton>
+                        </ListItem>
+                    );
+                })}
+            </List>
+        </MuiDrawer>
     );
 }
 
