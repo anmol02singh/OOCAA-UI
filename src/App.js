881a6d8b195802c2bdaf0f86d18f924999e55413
Anmol Singh <128087089+anmol02singh@users.noreply.github.com>
2024-12-29 19:36:30 -0500

    Kan 24/dashboard (#8)
    * Updates json, implementing Full Calender, Nivo etc
    
    * mockdata, index update and theme
    
    * TopBar and BarChart
    
    * finished dashbord
    
    * Finished Dashboard and Navbar
    
    * Converted absolute file paths to relative
    
    ---------
    
    Co-authored-by: Sanchit Duggal <san249@my.yorku.ca>
    Co-authored-by: Natalie Dean <natalierdean1@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,50 +1,44 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+//import "/Users/san/OOCAA-UI/src/styles/index.css";
 import Home from "./pages/Home.tsx";
 import About from "./pages/About.tsx";
 import Contact from "./pages/Contact.tsx";
-import UserSignUp from "./pages/UserSignUp.tsx";
-import routes from "./routes";
-import UserLogin from "./pages/UserLogin.tsx";
-import NavbarLayout from "./components/NavbarLayout.tsx";
+import Navbar from "./scenes/global/Navbar.tsx";
+import { ColorModeContext, useMode } from "./theme.tsx";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar.tsx";
+import Bar from "./scenes/Bar/index.tsx";
+import Pie from "./scenes/Pie/index.tsx";
+import Line from "./scenes/Line/index.tsx";
+import Geo from "./scenes/Geo/index.tsx";
+import Dashboard from "./scenes/dashboard/index.tsx";
 
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<UserSignUp />} />
-      </Routes>
-      <Routes>
-        <Route path="/login" element={<UserLogin />} />
-      </Routes>
-      <Routes>
-        <Route
-          path={routes.home}
-          element={
-            <NavbarLayout>
-              <Home />
-            </NavbarLayout>
-          }
-        />
-        <Route
-          path={routes.about}
-          element={
-            <NavbarLayout>
-              <About />
-            </NavbarLayout>
-          }
-        />
-        <Route
-          path={routes.contact}
-          element={
-            <NavbarLayout>
-              <Contact />
-            </NavbarLayout>
-          }
-        />
-      </Routes>
-    </Router>
-  );
-};
+function App() {
+    const [theme, colorMode] = useMode();
+    const [isNavbar, setIsNavbar] = useState(true);
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <div className="app">
+                  <Navbar isNavbar = {isNavbar}/>   
+                    <main className="content">
+                        <Topbar setIsNavbar={setIsNavbar} />
+                        
+                        <Routes>
+                            <Route path="/" element={<Dashboard />} />
+                            <Route path="/bar" element={<Bar />} />
+                            <Route path="/pie" element={<Pie />} />
+                            <Route path="/line" element={<Line />} />
+                            <Route path="/geo" element={<Geo />} />
+                        </Routes>
+                    </main>
+                </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
+}
 
 export default App;
